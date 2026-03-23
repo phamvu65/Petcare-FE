264640f0ad40a704ea60d46ac7710ee81af496fc
= <=phamvu65cs@gmail.com>
2026-03-23 14:35:03 +0700

    done

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -7,8 +7,8 @@ import type {
 } from "axios";
 
 const api: AxiosInstance = axios.create({
-  // baseURL: "https://petcare-backend-api.onrender.com", 
-  baseURL: "http://localhost:8080", 
+   baseURL: "https://petcare-backend-api.onrender.com", 
+  // baseURL: "http://localhost:8080", 
   timeout: 5000,
   headers: {
     "Content-Type": "application/json",
